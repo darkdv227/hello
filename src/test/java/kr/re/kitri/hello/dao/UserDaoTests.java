323c3892eb9d54155983e18afdf35d61e67e7794
KITRI <lee@g.g>
2020-05-28 17:17:39 +0900

    단위 테스트 및 로깅 처리

---
+++ src/test/java/kr/re/kitri/hello/dao/UserDaoTests.java
@@ -0,0 +1,36 @@
+package kr.re.kitri.hello.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import kr.re.kitri.hello.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class UserDaoTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testInsertUser() {
+        User user = new User("ryu","hyunjin", 32);
+        userDao.insertUser(user);
+    }
+
+    @Test
+    public void testSelectAllUsers() {
+        List<User> users = userDao.selectAllUsers();
+        assertTrue(users.size() > 0);
+
+    }
+
+    @Test
+    public void testSelectUserByUserId() {
+        User user = userDao.selectUserByUserId("ryu");
+        assertEquals("hyunjin", user.getName());
+    }
+}
